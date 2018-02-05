3106c66a77f14f8814749bf763b549b2abdaa15c
george <thisbelongstogeorge@gmail.com>
2018-02-05 18:55:32 +0000

    WIP for meeting

--- nltk-test.py
+++ nltk-test.py
@@ -3,31 +3,36 @@ import io
 import sys
 from bllipparser import RerankingParser
 from anytree import Node, RenderTree
+import time
 
 rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
 
 f1 = io.open(sys.argv[1], "r+").read().splitlines()
 f2 = io.open(sys.argv[2], "r+").read().splitlines()
 
-trees1 = []
-trees2 = []
+def pruneTree(tree):
+    newT = ""
+    pc = ""
+    for c in tree:
+        #do thing to strip tree
+        newT+= c
+ 
 
-def treeParse(tString):
-    return []
+newTree = rrp.simple_parse("Here's a more simple sentence. This second part might be harder.")
+
 
 
 for tweet in f1:
     newTweet = tweet.encode('ascii', 'ignore')
-    print newTweet
     if len(newTweet)!=0:
         newTree = rrp.simple_parse(newTweet)
-        trees1.append(newTree)
+        print newTree
+        time.sleep(0.1)
 
 print trees1
 
 for tweet in f2:
     newTweet = tweet.encode('ascii', 'ignore')
-    print newTweet
     if len(newTweet)!=0:
         newTree = rrp.simple_parse(newTweet)
         trees2.append(newTree)
